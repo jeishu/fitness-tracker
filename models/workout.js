930b0303ba602b0322954e0945c28768ca901ec8
jeishu <jeremy.zhu@utexas.edu>
2021-02-24 17:36:53 -0600

    fixed some models and added morgan dependency

--- models/workout.js
+++ models/workout.js
@@ -2,22 +2,48 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
+const workoutSchema = new Schema({
+  day: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now()
+  },
+
+  exercise: [{
+    name: {
+      type: String,
+      trim: true,
+      required: "Whats the exercise called?"
+    },
+    type: {
+      type: String,
+      trim: true,
+      required: "What type of exercise is it?"
+    },
+    duration: {
+      type: Number,
+      trim: true,
+      require: "What is the duration of the exercise?"
+    }, 
+    distance: {
+      type: Number,
+      trim: true
+    }, 
+    weight: {
+      type: Number,
+      trim: true
+    }, 
+    reps: {
+      type: Number,
+      trim: true
+    },  
+    sets: {
+      type: Number,
+      trim: true
+    }
+  }]
+   
 });
 
-const Transaction = mongoose.model("Transaction", transactionSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
-module.exports = Transaction;
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const db = require("./models");
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {useNewUrlParser: true});
 
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
